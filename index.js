2185898183eea97373600df1764097c877439ea2
github username <github email address>
2019-02-25 15:00:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Strings
 const farmAnimals = 'cow horse sheep pig chicken'
 // 1. Use destructuring to assign appropriate varibles based on the sounds animals make.
-const [moo, neigh, baa, oink, cluck] = 'cow horse sheep pig chicken'.split(" ")
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ")
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
-const [Bessie, , Dolly, Babe, Little] = 'cow horse sheep pig chicken'.split(" ")
+const [Bessie, , Dolly, Babe, Little] = farmAnimals.split(" ")
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
-const [black_and_white, , black, pink] = 'cow horse sheep pig chicken'.split(" ")
+const [black_and_white, , black, pink] = farmAnimals.split(" ")
 
 // Arrays
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
@@ -17,7 +17,7 @@ const [red, orange, yellow, green, blue, indigo, violet] = colors
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 const [r, o, y, g, b, , v] = colors
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
 const [ , , , , , indg, ] = colors
 
 // Objects
@@ -47,5 +47,5 @@ const k_muppet = {
   k_partner: 'Miss Piggy'
 };
 
-const { song_2, song_4} = muppet.k_album_the_muppet_movie
-const { k_color, k_job, k_partner } = muppet
+const { song_2, song_4} = k_muppet.k_album.the_muppet_movie
+const { k_color, k_job, k_partner } = k_muppet
